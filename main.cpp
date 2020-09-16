7abc6a4b392bd031ddb74461ae051f7308a69cbc
zubat187 <krotilia3@gmail.com>
2020-09-16 18:21:47 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int q;
+int t;
+int I;
+cout <<"VVedite zaryad Q" <<endl;
+cin >> q;
+cout <<"VVedite vremya T" <<endl;
+cin >> t;
+cout <<"Sila toka I =" <<q/t <<endl;
+return 0;
+
+}
